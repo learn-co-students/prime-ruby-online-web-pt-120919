e70ff92ae81ba9b1b4a2ce8e51c19145074f714a
travis Lawson <travislawclimb@gmail.com>
2019-12-23 08:05:44 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+ def is_prime n
+  for d in 2..(n - 1)
+   if (n % d) == 0
+    return false
+   end
+  end
+
+  true
+ end
+
+ def is_prime2 n
+  foundDivider = false
+   for d in 2..(n - 1)
+    foundDivider = ((n % d) == 0) or foundDivider
+   end
+  not foundDivider
+ end
+ruby
